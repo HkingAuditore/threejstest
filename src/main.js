c01e367447e7de8478a1a291991658c3a9a0d820
hkinghuang <178854663@qq.com>
2023-05-23 16:32:34 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,27 +1,85 @@
-// Hello Cube App
-// Your first Three.js application
-// sizes
-const width = window.innerWidth
-const height = window.innerHeight
-// scene
-const scene = new THREE.Scene()
-scene.background = new THREE.Color(0x262626)
-// camera
-const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 100)
-camera.position.set(0, 0, 10)
-// cube
-const geometry = new THREE.BoxGeometry(2, 2, 2)
-const material = new THREE.MeshBasicMaterial({
-   color: 0xffffff,
-   wireframe: true
-})
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
-// renderer
-const renderer = new THREE.WebGL1Renderer()
-renderer.setSize(width, height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-// rendering the scene
-const container = document.querySelector('#threejs-container')
-container.append(renderer.domElement)
-renderer.render(scene, camera)
+let mesh, renderer, scene, camera;
+
+let gui;
+
+let lightProbe;
+let directionalLight;
+
+// linear color space
+const API = {
+    lightProbeIntensity: 1.0,
+    directionalLightIntensity: 0.2,
+    envMapIntensity: 1
+};
+
+init();
+
+function init() {
+
+    // renderer
+    renderer = new THREE.WebGLRenderer( { antialias: true } );
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    // tone mapping
+    renderer.toneMapping = THREE.NoToneMapping;
+
+
+    // scene
+    scene = new THREE.Scene();
+
+    // camera
+    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 1000 );
+    camera.position.set( 0, 0, 30 );
+
+
+
+    // probe
+    lightProbe = new THREE.LightProbe();
+    scene.add( lightProbe );
+
+    // light
+    directionalLight = new THREE.DirectionalLight( 0xffffff, API.directionalLightIntensity );
+    directionalLight.position.set( 10, 10, 10 );
+    scene.add( directionalLight );
+
+
+    const geometry = new THREE.SphereGeometry( 5, 64, 32 );
+    const material = new THREE.MeshStandardMaterial( {
+        color: 0xffffff,
+        metalness: 0,
+        roughness: 0,
+        envMapIntensity: API.envMapIntensity,
+    } );
+
+    // mesh
+    mesh = new THREE.Mesh( geometry, material );
+    scene.add( mesh );
+
+    render();
+
+
+
+
+    // listener
+    window.addEventListener( 'resize', onWindowResize );
+
+}
+
+function onWindowResize() {
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    render();
+
+}
+
+function render() {
+
+    renderer.render( scene, camera );
+
+}
