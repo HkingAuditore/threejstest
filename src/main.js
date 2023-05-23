83af6d6fdfbb0a1648768b2f04a996f1f2005da0
hkinghuang <178854663@qq.com>
2023-05-23 16:18:02 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,26 +1,27 @@
-import * as THREE from './three.js';
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-function animate() {
-	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
-}
-
-animate();
+// Hello Cube App
+// Your first Three.js application
+// sizes
+const width = window.innerWidth
+const height = window.innerHeight
+// scene
+const scene = new THREE.Scene()
+scene.background = new THREE.Color(0x262626)
+// camera
+const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 100)
+camera.position.set(0, 0, 10)
+// cube
+const geometry = new THREE.BoxGeometry(2, 2, 2)
+const material = new THREE.MeshBasicMaterial({
+   color: 0xffffff,
+   wireframe: true
+})
+const cube = new THREE.Mesh(geometry, material)
+scene.add(cube)
+// renderer
+const renderer = new THREE.WebGL1Renderer()
+renderer.setSize(width, height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+// rendering the scene
+const container = document.querySelector('#threejs-container')
+container.append(renderer.domElement)
+renderer.render(scene, camera)
